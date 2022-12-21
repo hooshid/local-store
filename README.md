3b1cb5416ce84ced687daf82f2d2dd255373d875
Alireza Kamali <arka1373@gmail.com>
2022-12-21 08:38:11 +0330

    update

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # local-store
-localStorage with expire time
+Helpers to make local storage easier to use and just like cookie, having expire time.
+
+## Usage
+
+```js
+import localStore from '@hooshid/localStore'
+```
+
+## Methods
+
+- `set` — Add item to local storage.
+- `get` — Get item from local storage
+- `remove` — Remove item from local storage.
+- `clearAll` — Removes all or just Prefixed items from local storage.
+- `has` — Check item exist in local storage.
+
+### Set
+
+Set an item in local storage.
+
+```js
+set(key: string, value: string, ttl?: number | string): void
+```
+
+- `key` - The key of the item to set.
+- `value` - The value to set.
+- `ttl` - The time life in seconds format for living cache in localStorage.
+- `ttl` - The time life in default is one year, if you want set time life you should set in secconds or set like below format.
+
+```bash
+m for minute
+h for hour
+d for day
+```
+
+#### examples
+* 60m -> 60 minute
+* 1h -> 1 hour (equal to 60m)
+* 24h -> 24 hours (equal to 1d)
+* 1d -> 1 day
+* 30d -> 30 days
+
+just use 1 above method and dont mixed (1d12h does not work -> 36h work fine!)
+
+### Get
+
+Get an item from local storage.
+
+```js
+get(key: string): string | null
+```
+
+- `key` — The key of the item to get.
+
+### Remove
+
+Remove item from local storage.
+
+```js
+remove(key: string): void
+```
+
+- `key` — The key of the item to remove.
+
+### Clear All
+
+Removes all or just Prefixed items from local storage.
+
+```js
+clearAll(prefix?: string): void
+```
+
+- `prefix` — if prefix provided, only keys start with prefix cleared and if prefix undefined all keys cleared.
+
+### Has
+
+Check key is exist
+
+```js
+has(key: string): boolean
+```
+
+- `key` — The key of the item.
---
+++ src/index.ts
@@ -0,0 +1,118 @@
+/**
+ * Set new key on localStorage
+ * @param key
+ * @param value
+ * @param ttl
+ */
+export function set(key: string, value: string, ttl?: number | string) {
+  // browser support ?
+  if (!window.localStorage || !key || !value) {
+    return;
+  }
+
+  const now = new Date();
+
+  // 365 day
+  if (!ttl) {
+    ttl = 60 * 60 * 24 * 365;
+  } else if (typeof ttl == "string") {
+    if (ttl.endsWith("m")) {
+      ttl = 60 * parseInt(ttl);
+    } else if (ttl.endsWith("h")) {
+      ttl = 60 * 60 * parseInt(ttl);
+    } else if (ttl.endsWith("d")) {
+      ttl = 60 * 60 * 24 * parseInt(ttl);
+    } else {
+      ttl = parseInt(ttl);
+    }
+  }
+
+  // `item` is an object which contains the original value
+  // as well as the time when it's supposed to expire
+  const item = {
+    value: value,
+    expire: now.getTime() + ttl * 1000,
+  };
+
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+/**
+ * Get an item from localStorage
+ * @param key
+ */
+export function get(key: string) {
+  // browser support ?
+  if (!window.localStorage || !key) {
+    return null;
+  }
+
+  const itemStr = localStorage.getItem(key);
+
+  // if the item doesn't exist, return null
+  if (!itemStr) {
+    return null;
+  }
+
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+
+  // compare the expire time of the item with the current time
+  if (now.getTime() > item.expire) {
+    // if the item is expired, delete the item from storage
+    // and return null
+    localStorage.removeItem(key);
+    return null;
+  }
+
+  return item.value as string;
+}
+
+/**
+ * Remove item from localStorage
+ * @param key
+ */
+export function remove(key: string) {
+  localStorage.removeItem(key);
+}
+
+/**
+ * Remove all localStorage keys with/without prefix
+ * @param prefix
+ */
+export function clearAll(prefix?: string) {
+  if (prefix) {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if ((localStorage.key(i) as string).indexOf(prefix) === 0) {
+          localStorage.removeItem(localStorage.key(i) as string);
+        }
+      }
+    }
+  } else {
+    localStorage.clear();
+  }
+}
+
+/**
+ * Check key is exist
+ * @param key
+ */
+export function has(key: string) {
+  if (get(key)) {
+    return true;
+  }
+
+  return false;
+}
+
+/**
+ * Local storage with expiration
+ */
+export default {
+  set,
+  get,
+  remove,
+  clearAll,
+  has,
+};
